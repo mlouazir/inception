0b0d0e548e84e85de30589c45a4f50a85fc47bbd
Mohamed Louazir <mlouazir@e2r6p5.1337.ma>
2025-04-16 15:31:13 +0100

    ni: To test in a VM

--- srcs/mariadb/conf/init.sh
+++ srcs/mariadb/conf/init.sh
@@ -2,8 +2,6 @@
 
 service mariadb start
 
-sleep 6
-
 mariadb -e "SET PASSWORD FOR 'root'@'localhost'  = PASSWORD('root')"
 mariadb -e "CREATE DATABASE wp_data"
 mariadb -e "CREATE USER 'wp'@'%' IDENTIFIED BY 'wp'"
@@ -11,5 +9,3 @@ mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'wp'"
 mariadb -e "FLUSH PRIVILEGES"
 
 service mariadb stop
-
-sleep 4
--- srcs/wordpress/config/init.sh
+++ srcs/wordpress/config/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd /var/www/html/wordpress
+cd /var/www/html/
 
-wp config create --allow-root --dbname=wp_data --dbuser=wp --dbpass=wp --dbhost=mariadb
+wp core download --allow-root
+
+wp config create --allow-root --dbname=wp_data --dbuser=wp --dbpass=wp --dbhost=mariadb:3306
+
+wp core install --allow-root --url=localhost --title=mySite --admin_user=admin --admin_password=admin --admin_email=admin@example.com
+
+wp db check --allow-root
+
+php-fpm8.2
