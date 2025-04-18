0c60d7971b0299777865b3aba13d9df2d0f42899
Mohamed Louazir <medlouazir1337@gmail.com>
2025-04-18 20:02:27 +0100

    ni:

--- srcs/wordpress/config/init.sh
+++ srcs/wordpress/config/init.sh
@@ -6,7 +6,7 @@ wp core download --allow-root
 
 wp config create --allow-root --dbname=wp_data --dbuser=wp --dbpass=wp --dbhost=mariadb:3306
 
-wp core install --allow-root --url=http://localhost:8080 --title=mySite --admin_user=boss --admin_password=boss --admin_email=admin@example.com
+wp core install --allow-root --url=https://localhost --title=mySite --admin_user=boss --admin_password=boss --admin_email=admin@example.com
 
 wp user create --allow-root user user@user.com --user_pass=user
 
