5b20c60d7ef2eff4defb6a5ac9b33c7769281116
mlouazir <medlouazir1337@gmail.com>
2025-05-02 09:20:38 +0100

    feat: Corrected Directory Structure

--- srcs/nginx/conf/init-cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if find key.pem cert.pem; then
-    echo "Certificate Already Created"
-else
-    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=HH/ST=CALIFORNIA/L=TET1/O=MLOUAZIR/OU=ALL/CN=mlouazir.42.fr"
-fi
