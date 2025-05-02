d4012f56d9d039610a2484e62423603940d291a6
mlouazir <medlouazir1337@gmail.com>
2025-05-02 09:25:02 +0100

    feat: Corrected Directory Structure

---
+++ srcs/requirements/wordpress/conf/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WP_DB_PSSWD=$(</run/secrets/wp_db_psswd)
+WP_ADMIN_PSSWD=$(</run/secrets/wp_admin_psswd)
+WP_USER_PSSWD=$(</run/secrets/wp_user_psswd)
+
+wp core download --allow-root
+
+wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$WP_USER --dbpass=$WP_DB_PSSWD --dbhost=mariadb:3306
+
+wp core install --allow-root --url=https://localhost --title=mySite --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PSSWD --admin_email=$WP_ADMIN_EMAIL
+
+wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_USER_PSSWD
+
+wp plugin install redis-cache --activate --allow-root
+
+wp config set WP_REDIS_HOST redis --allow-root
+
+wp redis enable --allow-root
+
+php-fpm7.4 -F
